533132c75e1feecdda71242019de0cd54517e8ed
Mike_Le <iam@mike-le.info>
2024-01-10 21:06:09 +0100

    basic-login page

--- fe/src/pages/login/index.tsx
+++ fe/src/pages/login/index.tsx
@@ -1,5 +1,73 @@
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBRow,
+} from "mdb-react-ui-kit";
+import pc from "../../assets/images/pc.jpg";
+
+import "./Login.css";
+
 function LoginPage(): JSX.Element {
-  return <div data-testid="login-page">login page</div>;
+  return (
+    <MDBContainer className="my-5" data-testid="login-page">
+      <MDBCard>
+        <MDBRow className="g-0">
+          <MDBCol md="6">
+            <MDBCardImage
+              src={pc}
+              alt="login form"
+              className="rounded-start w-100"
+            />
+          </MDBCol>
+
+          <MDBCol md="6">
+            <MDBCardBody className="d-flex flex-column">
+              <div className="d-flex flex-row mt-2">
+                <MDBIcon
+                  fas
+                  icon="cubes fa-3x me-3"
+                  style={{ color: "#ff6219" }}
+                />
+                <span className="h1 fw-bold mb-0">Logo</span>
+              </div>
+
+              <h5
+                className="fw-normal my-4 pb-3"
+                style={{ letterSpacing: "1px" }}
+              >
+                Sign into your account
+              </h5>
+
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Email address"
+                id="formControlLg"
+                type="email"
+                size="lg"
+              />
+              <MDBInput
+                wrapperClass="mb-4"
+                label="Password"
+                id="formControlLg"
+                type="password"
+                size="lg"
+              />
+
+              <MDBBtn className="mb-4 px-5" color="dark" size="lg">
+                Login
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCol>
+        </MDBRow>
+      </MDBCard>
+    </MDBContainer>
+  );
 }
 
 export default LoginPage;
