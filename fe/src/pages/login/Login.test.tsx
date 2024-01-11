fe9a472475c4faad11923214db0975b879e1b629
Mike_Le <iam@mike-le.info>
2024-01-11 04:51:19 +0100

    login input value test

---
+++ fe/src/pages/login/Login.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import App from "../../App";
+import { setMealForm } from "../../store/generalSlice"; // import the action creator
+import { store } from "../../store/store";
+
+test("input value reflects state update", () => {
+  store.dispatch(setMealForm({ key: "email", value: "test@example.com" }));
+  store.dispatch(setMealForm({ key: "password", value: "123456Asd" }));
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/login"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  const emailInput = screen.getByLabelText("Email address") as HTMLInputElement;
+  expect(emailInput.value).toBe("test@example.com");
+  const passwordInput = screen.getByLabelText("Password") as HTMLInputElement;
+  expect(passwordInput.value).toBe("123456Asd");
+});
