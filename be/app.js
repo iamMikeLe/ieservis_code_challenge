c1bd961888e20621ea78e10635d311d6a5112846
Mike_Le <iam@mike-le.info>
2024-01-13 10:05:02 +0100

    remove not needed CORS

--- be/app.js
+++ be/app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
 
   next();
 });
