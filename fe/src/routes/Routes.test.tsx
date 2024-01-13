41af666545c43782323dd9c913f9a07065cd9b0a
Mike_Le <iam@mike-le.info>
2024-01-13 14:24:13 +0100

    tests optimalization

--- fe/src/routes/Routes.test.tsx
+++ fe/src/routes/Routes.test.tsx
@@ -1,101 +1,62 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
-import configureMockStore from "redux-mock-store";
+import configureStore from "redux-mock-store";
 import App from "../App";
 
-const mockStore = configureMockStore();
-const store = mockStore({
-  general: {
-    userData: {},
-  },
-  login: {
-    loginFormValues: {
-      email: "",
-      password: "",
-    },
-  },
-});
+const mockStore = configureStore([]);
 
-function customRender(ui: React.ReactElement, { route = "/" } = {}) {
-  function Wrapper({ children }: { children: React.ReactNode }) {
-    return (
-      <Provider store={store}>
-        <MemoryRouter initialEntries={[route]}>{children}</MemoryRouter>
-      </Provider>
-    );
-  }
-  return render(ui, { wrapper: Wrapper });
+function renderWithStoreAndRouter(
+  userData: { type: string },
+  initialRoute: string
+) {
+  const store = mockStore({
+    general: {
+      userData,
+    },
+    login: {
+      loginFormValues: {
+        email: "",
+        password: "",
+      },
+    },
+  });
+
+  return render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[initialRoute]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  );
 }
 
-// ------------------------------
+test("Redirects to login page on /", () => {
+  renderWithStoreAndRouter({ type: "" }, "/");
+  const loginElement = screen.getByTestId("login-page");
+  expect(loginElement).toBeInTheDocument();
+});
 
 test("renders Admin login page on /admin/login", () => {
-  customRender(<App />, { route: "/admin/login" });
-
+  renderWithStoreAndRouter({ type: "" }, "/admin/login");
   const loginElement = screen.getByTestId("admin-login");
   expect(loginElement).toBeInTheDocument();
 });
 
-// ------------------------------
-
-test("renders login page on /login", () => {
-  customRender(<App />, { route: "/login" });
-
+test("renders login page on /login route", () => {
+  renderWithStoreAndRouter({ type: "" }, "/login");
   const loginElement = screen.getByTestId("login-page");
   expect(loginElement).toBeInTheDocument();
 });
 
-// ------------------------------
-
-test("Redirects to login page on /", () => {
-  customRender(<App />, { route: "/" });
-
-  const loginElement = screen.getByTestId("login-page");
-  expect(loginElement).toBeInTheDocument();
-});
-
-// ------------------------------
-
-test("renders ImgToPdf page on /images-to-pdf", () => {
-  const store = mockStore({
-    general: {
-      userData: {
-        type: "user",
-      },
-    },
-  });
-
-  render(
-    <Provider store={store}>
-      <MemoryRouter initialEntries={["/images-to-pdf"]}>
-        <App />
-      </MemoryRouter>
-    </Provider>
-  );
-
+test("renders ImgToPdf page on /images-to-pdf route when userData.type exists", () => {
+  renderWithStoreAndRouter({ type: "user" }, "/images-to-pdf");
   const loginElement = screen.getByTestId("img-to-pdf-page");
   expect(loginElement).toBeInTheDocument();
 });
 
-// ------------------------------
-
 test("renders error page on invalid route", () => {
-  const store = mockStore({
-    general: {
-      userData: {
-        type: "user",
-      },
-    },
-  });
-  render(
-    <Provider store={store}>
-      <MemoryRouter initialEntries={["/someInvalid-route"]}>
-        <App />
-      </MemoryRouter>
-    </Provider>
-  );
-
+  renderWithStoreAndRouter({ type: "user" }, "/someInvalid-route");
   const loginElement = screen.getByTestId("error-page");
   expect(loginElement).toBeInTheDocument();
 });
