50d752d73f248972a19caef66d1b53115204ba3d
Mike_Le <iam@mike-le.info>
2024-01-15 21:19:53 +0100

    adminLogin test

---
+++ fe/src/pages/adminLogin/AdminLogin.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import App from "../../App";
+
+import { store } from "../../store/store";
+import { setAdminLoginForm } from "./adminLoginSlice";
+
+vi.mock("../../API/userAPI", () => ({
+  apiRequest: vi.fn(),
+  getMaintenanceStatus: vi.fn().mockResolvedValue(false),
+}));
+
+describe("When not logged in", () => {
+  beforeEach(async () => {
+    store.dispatch(
+      setAdminLoginForm({ key: "email", value: "test@example.com" })
+    );
+    store.dispatch(setAdminLoginForm({ key: "password", value: "123456Asd" }));
+
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/admin/login"]}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+    });
+  });
+
+  test("input value reflects state update", () => {
+    const emailInput = screen.getByLabelText(
+      "Email address"
+    ) as HTMLInputElement;
+    expect(emailInput.value).toBe("test@example.com");
+    const passwordInput = screen.getByLabelText("Password") as HTMLInputElement;
+    expect(passwordInput.value).toBe("123456Asd");
+  });
+});
