391a96b31d275da86f60ba08d52039c5aaa07a50
Mike_Le <iam@mike-le.info>
2024-01-16 08:44:53 +0100

    img to upload functionality raw

--- fe/src/components/convertor/index.tsx
+++ fe/src/components/convertor/index.tsx
@@ -1,12 +1,74 @@
-import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import { jsPDF } from "jspdf";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
 function Convertor(): JSX.Element {
+  const [images, setImages] = useState<Array<string>>([]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setImages((prevImages) => [...prevImages, reader.result as string]);
+      };
+
+      reader.readAsDataURL(file);
+    });
+  }, []);
+
+  const generatePdf = () => {
+    const pdf = new jsPDF();
+    const WIDTH = 210; // width of A4 in mm
+    const HEIGHT = 297; // height of A4 in mm
+
+    const loadImages = images.map((image, index) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = image;
+        img.onload = function () {
+          const width = img.width;
+          const height = img.height;
+          const aspectRatio = width / height;
+
+          let pdfWidth = WIDTH;
+          let pdfHeight = WIDTH / aspectRatio;
+
+          if (pdfHeight > HEIGHT) {
+            pdfHeight = HEIGHT;
+            pdfWidth = HEIGHT * aspectRatio;
+          }
+
+          pdf.addImage(image, "JPEG", 0, 0, pdfWidth, pdfHeight);
+          if (index < images.length - 1) {
+            pdf.addPage();
+          }
+          resolve(null);
+        };
+        img.onerror = reject;
+      });
+    });
+
+    Promise.all(loadImages)
+      .then(() => {
+        pdf.save("download.pdf");
+      })
+      .catch((error) => {
+        console.error("Error generating PDF", error);
+      });
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div data-testid="convertor">
-      <MDBCard className="convertor-box">
-        <MDBCardBody>
-          <h6>Upload section</h6>
-        </MDBCardBody>
-      </MDBCard>
+    <div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <button onClick={generatePdf} disabled={images.length === 0}>
+        Generate PDF
+      </button>
     </div>
   );
 }
