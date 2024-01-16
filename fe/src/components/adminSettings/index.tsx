e74947490ad39e3bd9cc943b8b627e73663cd795
Mike_Le <iam@mike-le.info>
2024-01-16 14:18:57 +0100

    set max images ui

--- fe/src/components/adminSettings/index.tsx
+++ fe/src/components/adminSettings/index.tsx
@@ -1,9 +1,18 @@
-import { MDBCard, MDBCardBody, MDBCheckbox } from "mdb-react-ui-kit";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCheckbox,
+  MDBCol,
+  MDBInput,
+  MDBRow,
+} from "mdb-react-ui-kit";
 import { useCallback, useState } from "react";
 import toast from "react-hot-toast";
 import { updateMaintenanceMode } from "../../API/userAPI";
 import { selectMaintenance, setMaintenance } from "../../store/generalSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import "./AdminSettings.css";
 
 function AdminSettings(): JSX.Element {
   const [loading, setLoading] = useState(false);
@@ -35,14 +44,27 @@ function AdminSettings(): JSX.Element {
   return (
     <MDBCard className="convertor-box" data-testid="admin-settings">
       <MDBCardBody>
-        <h6>Admin app settings</h6>
-        <MDBCheckbox
-          disabled={loading}
-          id="controlledCheckbox"
-          label="Lock app - put it in maintenance mode (users can't login)"
-          checked={isUnderMaintenance}
-          onChange={handleCheckboxChange}
-        />
+        <MDBRow>
+          <MDBCol md="12" className="admin-settings-title">
+            <h5>Admin app settings</h5>
+            <hr />
+          </MDBCol>
+          <MDBCol md="6">
+            <h6>App lock</h6>
+            <MDBCheckbox
+              disabled={loading}
+              id="controlledCheckbox"
+              label="Turn on/off maintenance mode (users can't login)"
+              checked={isUnderMaintenance}
+              onChange={handleCheckboxChange}
+            />
+          </MDBCol>
+          <MDBCol md="6">
+            <h6>Set max allowed images to be uploaded</h6>
+            <MDBInput label="Max images" id="typeNumber" type="number" />
+            <MDBBtn style={{ marginTop: "0.5rem" }}>Submit</MDBBtn>
+          </MDBCol>
+        </MDBRow>
       </MDBCardBody>
     </MDBCard>
   );
