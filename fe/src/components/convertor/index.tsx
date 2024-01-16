52941e8b723e6d514d187f582f90052a5fe28089
Mike_Le <iam@mike-le.info>
2024-01-16 04:32:00 +0100

    convertor component

---
+++ fe/src/components/convertor/index.tsx
@@ -0,0 +1,14 @@
+import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+function Convertor(): JSX.Element {
+  return (
+    <div data-testid="convertor">
+      <MDBCard className="convertor-box">
+        <MDBCardBody>
+          <h6>Upload section</h6>
+        </MDBCardBody>
+      </MDBCard>
+    </div>
+  );
+}
+
+export default Convertor;
--- fe/src/pages/imgToPdf/index.tsx
+++ fe/src/pages/imgToPdf/index.tsx
@@ -1,6 +1,5 @@
-import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
-
 import AdminSettings from "../../components/adminSettings";
+import Convertor from "../../components/convertor";
 import { UserData, selectUserData } from "../../store/generalSlice";
 import { useAppSelector } from "../../store/hooks";
 import "./ImgToPdf.css";
@@ -12,11 +11,7 @@ function ImgToPdf(): JSX.Element {
     <div data-testid="img-to-pdf-page">
       {userData?.type === "admin" && <AdminSettings />}
 
-      <MDBCard className="convertor-box">
-        <MDBCardBody>
-          <h6>Upload section</h6>
-        </MDBCardBody>
-      </MDBCard>
+      <Convertor />
     </div>
   );
 }
