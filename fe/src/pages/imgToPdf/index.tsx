2ce8d2e103fdddd25fccc8853ff46bccc2106f53
Mike_Le <iam@mike-le.info>
2024-01-16 04:10:19 +0100

    add type

--- fe/src/pages/imgToPdf/index.tsx
+++ fe/src/pages/imgToPdf/index.tsx
@@ -1,12 +1,12 @@
 import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
 
 import AdminSettings from "../../components/adminSettings";
-import { selectUserData } from "../../store/generalSlice";
+import { UserData, selectUserData } from "../../store/generalSlice";
 import { useAppSelector } from "../../store/hooks";
 import "./ImgToPdf.css";
 
 function ImgToPdf(): JSX.Element {
-  const userData = useAppSelector(selectUserData);
+  const userData = useAppSelector<UserData | null>(selectUserData);
 
   return (
     <div data-testid="img-to-pdf-page">
