29c0f83a00067145c91d781eed5a7cc5aedd15fc
Mike_Le <iam@mike-le.info>
2024-01-23 13:57:13 +0100

    basic render test

---
+++ fe/src/main.test.tsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import { persistor, store } from "./store/store";
+
+test("renders without crashing", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  );
+});
